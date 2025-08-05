72f998f357faa5d2eabd402b188cdc8606c669d3
John Ray Judaya <judayajohnray@gmail.com>
2025-08-05 06:35:49 +0000

    #1 feat: setup dev environment

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,20 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        cors: true,
-    },
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+            port: 5173,
+        },
+        // Allow CORS from specific dev origins.
+        // Add your own local domain (or port) here if you're using a different setup.
+        // Useful when running Vite behind a reverse proxy or using custom local domains.
+        cors: {
+            origin: ['http://grd-tsui.local', 'http://localhost:8080'],
+            credentials: true
+        }
+    }
 });
